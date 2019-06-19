26cbeff3f1f423d618733ef69dcf29901de064f4
MagoDelBlocco <bgdnkln@gmail.com>
2019-06-19 20:12:43 +0300

    Fully implemented Hidden Layer

--- include/NeuralNetwork.h
+++ include/NeuralNetwork.h
@@ -11,7 +11,8 @@ class NeuralNetwork {
    public:
     NeuralNetwork(int _inputNeurons, int _hiddenLayers,
                   std::vector<int> _hiddenLayersSizes, int _outputNeurons,
-                  double _learningRate) {};
+                  double _learningRate) {
+    };
                   
     void Train(const std::vector<int> &input, int correctValue) {};
 
@@ -25,34 +26,70 @@ class NeuralNetwork {
         
         HiddenLayer(const size_t &_size, const size_t &_nextLayerSize) {}
         
-        HiddenLayer(const HiddenLayer &target) {}
+        HiddenLayer(const HiddenLayer &target) {
+            *this = target;
+        }
         
-        HiddenLayer(HiddenLayer &&target) {}
+        HiddenLayer(HiddenLayer &&target) {
+            *this = target;
+        }
         
-        HiddenLayer &operator=(const HiddenLayer &target) {}
+        HiddenLayer &operator=(const HiddenLayer &target) {
+            size = target.size;
+            nextLayerSize = target.nextLayerSize;
+            activations = target.activation;
+            bias = target.bias;
+            weights = target.weights;
+        }
         
-        HiddenLayer &operator=(HiddenLayer &&target) {}
+        HiddenLayer &operator=(HiddenLayer &&target) {
+            size = std::move(target.size);
+            nextLayerSize = std::move(target.nextLayerSize);
+            activations = std::move(target.activations);
+            bias = std::move(target.bias);
+            weights = std::move(target.weights);
+        }
         
-        Matrix<long double> &GetActivations() {}
+        Matrix<long double> &GetActivations() {
+            return activations;
+        }
         
-        Matrix<biasType> &GetBias() {}
+        Matrix<biasType> &GetBias() {
+            return bias;
+        }
         
-        Matrix<long double> &GetWeights() {}
+        Matrix<long double> &GetWeights() {
+            return weights;
+        }
         
-        Matrix<long double> &&ComputeNextLayer() {}
+        Matrix<long double> &&ComputeNextLayer() {
+            Matrix<long double> retval;
+            auto sigma = [=, &retval] (const Matrix<long double> &target) {
+                for (auto &it : target)  /// TODO Rica
+                    it = sigmoid(it);
+            };
+            
+            retval = sigma(weights * activations + bias);
+        }
         
-        Matrix<long double> &&SetWeights(Matrix<long double> &&target) {}
+        Matrix<long double> &&SetWeights(Matrix<long double> &&target) {
+            weights = std::move(target);
+        }
         
-        Matrix<biasType> &&SetBias(Matrix<biasType> &&target) {}
+        Matrix<biasType> &&SetBias(Matrix<biasType> &&target) {
+            bias = std::move(target);
+        }
         
-        Matrix<long double> &&SetNextLayer(Matrix<long double> &&target) {}
+        Matrix<long double> &&SetActivations(Matrix<long double> &&target) {
+            activations = std::move(target);
+        }
         
         ~HiddenLayer() {}
 
     private:
         size_t size, nextLayerSize;
-        Matrix<long double> Activations, Weights;
-        Matrix<biasType> Bias;
+        Matrix<long double> activations, weights;
+        Matrix<biasType> bias;
     };
        
     int inputNeurons;
