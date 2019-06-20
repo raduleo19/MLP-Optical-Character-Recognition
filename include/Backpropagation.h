8589dcc38e43c1a90408203af66fbf4544743385
raduleo19 <radu.leonard.rica@gmail.com>
2019-06-20 23:24:00 +0300

    Backpropagation Adaptation To New Matrix Class

--- include/Backpropagation.h
+++ include/Backpropagation.h
@@ -9,25 +9,25 @@
 template <class Derivative>
 class Backpropagate {
    public:
-    void backpropagate(std::vector<Matrix<long double>> &weights,
-                       std::vector<Matrix<long double>> &biases,
-                       std::vector<Matrix<long double>> &activations,
+    void backpropagate(std::vector<Matrix> &weights,
+                       std::vector<Matrix> &biases,
+                       std::vector<Matrix> &activations,
                        const size_t &layersCount,
-                       const Matrix<long double> &desiredOutput,
+                       const Matrix &desiredOutput,
                        const long double &learningRate) {
-        std::vector<Matrix<long double>> dCdW(layersCount - 1);
-        std::vector<Matrix<long double>> dCdB(layersCount - 1);
+        std::vector<Matrix> dCdW(layersCount - 1);
+        std::vector<Matrix> dCdB(layersCount - 1);
 
         dCdB[layersCount - 2] =
             (activations[layersCount - 1] - desiredOutput)
                 .HadamardMultiply(activations[layersCount - 2] *
                                       weights[layersCount - 2] +
-                                  biases[layersCount - 2]);
-        dCdB[layersCount - 2].applyFunction<Derivative>();
+                                  biases[layersCount - 2])
+                .ApplyFunction<Derivative>();
 
         for (int i = layersCount - 3; i >= 0; i--) {
-            auto temp = activations[i] * weights[i] + biases[i];
-            temp.applyFunction<Derivative>();
+            auto temp = activations[i] * weights[i] +
+                        biases[i].ApplyFunction<Derivative>();
             dCdB[i] = (dCdB[i + 1] * weights[i + 1].Transpose())
                           .HadamardMultiply(temp);
         }
